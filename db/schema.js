9a57d9a583ee263de3830bd4d9a7c5587afdd433
GeoJoseph <josephgeo259@gmail.com>
2018-04-12 17:06:41 -0400

    objects showing on dbs

--- db/schema.js
+++ db/schema.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// // Use native promises
-// mongoose.Promise = global.Promise;
 
 const commentSchema = new Schema({
     description: String,
