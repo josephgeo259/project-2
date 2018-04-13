47f16560d793c0958d31ef982ba048565629bf36
GeoJoseph <josephgeo259@gmail.com>
2018-04-12 21:24:32 -0400

    made to many controllers deleted them

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: `General Manger's Playbook` });
+router.get('/', function(req, res) {
+  res.redirect('/gm');
 });
 
 module.exports = router;
